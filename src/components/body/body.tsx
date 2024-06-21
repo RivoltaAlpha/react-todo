0cbe37d7ba4b7534e28e2f47552116bf8170b675
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-06-21 15:18:45 +0300

    body

---
+++ src/components/body/body.tsx
@@ -0,0 +1,25 @@
+import { BodyProps } from '../../types/types';
+
+const Body= ({ todos, completeTodo, deleteTodo, updateTodo } :BodyProps) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <input 
+            type="checkbox" 
+            checked={todo.completed} 
+            onChange={() => completeTodo(todo.id)} 
+          />
+          <input 
+            type="text" 
+            value={todo.text} 
+            onChange={(e) => updateTodo(todo.id, e.target.value)} 
+          />
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Body;
